9eef222043e290613bffbab4330009a4dd69dbbc
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2023-04-04 16:15:49 +0400

    fix:corsmb

--- backend/constants/corsconfig.js
+++ backend/constants/corsconfig.js
@@ -1,5 +1,9 @@
 const corsOptions = {
-  origin: "http://ohheyfront.nomoredomains.work",
+  origin: [
+    "http://ohheyfront.nomoredomains.work",
+    "http://localhost",
+    "http://localhost:3000",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
   credentials: true,
