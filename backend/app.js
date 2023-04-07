58f9bf70af98c1a249bc565f3e81f48e7315dc13
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2023-04-07 14:18:28 +0400

    port fix

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewars/logger');
 const corsOptions = require('./constants/corsconfig');
 // require('dotenv').config();
 
-const { PORT } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
